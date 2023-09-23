6dd2e6042fbc914c237206b4c7feeda17119f0c7
Sabah <sabahalmel7@gmail.com>
2023-09-23 16:49:51 +0300

    step 3

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
-
+const moment = require("moment");
+const router = express.Router();
 const app = express();
+
+router.get("/", (req, res) => {
+  res.json("hello world");
+});
+
+router.get("/test", (req, res) => {
+  res.status(200).json({ message: "ok" });
+});
+
+router.get("/time", (req, res) => {
+  res.status(200).json({ message: moment().format() });
+});
+
+app.use("/", router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`ok`));
