15395511a21930efebe504cc6d5a644dce920ee3
Sabah <sabahalmel7@gmail.com>
2023-09-23 17:32:07 +0300

    step 4

--- index.js
+++ index.js
@@ -8,11 +8,32 @@ router.get("/", (req, res) => {
 });
 
 router.get("/test", (req, res) => {
-  res.status(200).json({ message: "ok" });
+  res.status(200).json({ status: 200, message: "ok" });
 });
 
 router.get("/time", (req, res) => {
-  res.status(200).json({ message: moment().format() });
+  res.status(200).json({ status: 200, message: moment().format() });
+});
+
+router.get("/hello/:id", (req, res) => {
+  res.status(200).json({ status: 200, message: "Hello, " + req.params.id });
+});
+
+router.get("/hello", (req, res) => {
+  res.status(200).json({ status: 200, message: "Hello" });
+});
+
+router.get("/search", (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.status(200).json({ status: 200, message: "ok", data: search });
+  } else {
+    res.status(500).json({
+      status: 500,
+      error: true,
+      message: "you have to provide a search",
+    });
+  }
 });
 
 app.use("/", router);
